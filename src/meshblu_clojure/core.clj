8adf1a7f9f451b7a6b3c6317d51737de1ed65550
redaphid <iam@aaronherres.com>
2016-01-31 22:13:49 -0700

    macros: how do they work?

--- src/meshblu_clojure/core.clj
+++ src/meshblu_clojure/core.clj
@@ -13,13 +13,29 @@
 })
 
 
+(defmacro hi [f options]
+  (list 'let ['url (list ':url options)]
+    (list f 'url)
+  )
+)
+
+(macroexpand '(hi println meshblu-json))
+
+(hi println {:url "hi"})
+
 (defn status [& [options]]
+  "get the current status of meshblu"
   (let [{:keys [url]} (merge meshblu-json options)]
    (:meshblu (:body (http/get (str url "/status") {:as :json})))
   )
 )
 
+(comment
+  (status meshblu-json)
+)
+
 (defn register [[& options]]
+  "register a device with meshblu"
   (let [{:keys [url data]} (merge meshblu-json options)]
     (:body (http/post (str url "/devices") {:content-type :json :form-params data :as :json}))
   )
